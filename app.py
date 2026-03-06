bec5d1bad9334285c161609f24d3c5166ead1267
siddarth182772-cloud <siddarth.182772@gmail.com>
2026-03-06 09:44:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+# Create window
+window = tk.Tk()
+window.title("My First Frontend App")
+window.geometry("400x200")
+
+# Function
+def show_message():
+    label.config(text="Hello Siddarth! Welcome to Python Frontend")
+
+# Label
+label = tk.Label(window, text="Click the button", font=("Arial", 14))
+label.pack(pady=20)
+
+# Button
+button = tk.Button(window, text="Click Me", command=show_message)
+button.pack()
+
+# Run application
+window.mainloop()
